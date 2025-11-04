c702e77704043a90d03b9e9ece9ec7c2de87907f
Kris Jenkins <kris.jenkins@snowflake.com>
2025-11-04 12:07:59 +0000

    Updating to v3.13.0 official.

--- overlays/snowflake-cli.nix
+++ overlays/snowflake-cli.nix
@@ -1,11 +1,11 @@
 final: prev: {
   snowflake-cli = prev.snowflake-cli.overridePythonAttrs (oldAttrs: rec {
-    version = "3.13.0-rc0";
+    version = "3.13.0";
     src = prev.fetchFromGitHub {
       owner = "snowflakedb";
       repo = "snowflake-cli";
       tag = "v${version}";
-      hash = "sha256-QRuhntct5vTgNeuHGpK34gO2XtfScO0vxE13ZN/qvOc=";
+      hash = "sha256-TOX3U81Rg16wT+5/qqIWJqMQfRQx8Ng2DtHlSAH0RR4=";
     };
     dependencies = (oldAttrs.dependencies or []) ++ (with prev.python3Packages; [
       prompt-toolkit
